7f3e18ac5fbf450fb6e49555a1d94092733fef03
Refa Andhika <refa.andhika@shopee.com>
2019-11-01 19:07:33 +0700

    Add google analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-83060526-2');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
